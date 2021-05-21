3783cd746fca7b2d41b7f2e8cc939832de2e3951
Haruki Kiyama <aladdin.magi101@gmail.com>
2021-05-21 04:43:56 +0000

    [Add] product index & show

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -4,7 +4,13 @@ class Public::CartProductsController < ApplicationController
     end
     
     def create
-    
+        @cart_product = CartProduct.new(post_product_params)
+        if @cart_product.save
+            redirect_to products_path
+        else
+            @cart_product.save!
+            redirect_to home_about_path
+        end
     end
     
     def update
@@ -17,5 +23,11 @@ class Public::CartProductsController < ApplicationController
     
     def destroy_all
     
+    end
+    
+    private
+    
+    def post_product_params
+        params.require(:cart_product).permit(:member_id, :product_id, :quantity)
     end
 end
