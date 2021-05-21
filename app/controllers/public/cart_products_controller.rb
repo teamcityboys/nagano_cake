54d5345ccb9829c02da6502944d13e7c04c489fd
dmmweb-ryota <mac.kihara.12@gmail.com>
2021-05-21 05:01:27 +0000

    make cart_destroy

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,10 +1,16 @@
 class Public::CartProductsController < ApplicationController
     def index
-        # @cart_products = cart_product.all
+
     end
 
     def create
-
+        @cart_product = CartProduct.new(post_product_params)
+        if @cart_product.save
+            redirect_to products_path
+        else
+            @cart_product.save!
+            redirect_to home_about_path
+        end
     end
 
     def update
@@ -21,5 +27,11 @@ class Public::CartProductsController < ApplicationController
         @cart_products = current_customer.cart_products
         @cart_products.destroy_all
         redirect_to cart_products
+    end
+
+    private
+
+    def post_product_params
+        params.require(:cart_product).permit(:member_id, :product_id, :quantity)
     end
 end
