73dc6726f76730f68e8ab2ffeced7d2a538c964f
dmmweb-ryota <mac.kihara.12@gmail.com>
2021-05-22 04:06:53 +0000

    fix models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,10 +3,10 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
+
   has_many :cart_products, dependent: :destroy,foreign_key: 'member_id'
 
-=======
+
   #フルネームメソッド
   def full_name
     self.last_name + self.first_name
@@ -16,10 +16,10 @@ class Customer < ApplicationRecord
   def full_name_kana
     self.last_name_kana + self.first_name_kana
   end
-  
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
->>>>>>> origin/develop
+
 end
