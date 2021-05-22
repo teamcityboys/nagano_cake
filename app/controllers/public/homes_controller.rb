c411bb372e3927415cf2351f6d91d517199b113a
Haruki Kiyama <aladdin.magi101@gmail.com>
2021-05-22 12:14:10 +0000

    [Fix] fic home layout

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @new_products = Product.order(:create_at)
+    @new_products = Product.order(:create_at).where.not(is_active: false)
   end
 
   def about
