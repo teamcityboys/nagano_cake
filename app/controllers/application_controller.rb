244d5bd2be4bc6b082ef341847d1b0cd83dfc344
dmmweb-ryota <mac.kihara.12@gmail.com>
2021-05-23 05:32:34 +0000

    サインアップ時のエラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
       before_action :configure_permitted_parameters, if: :devise_controller?
-      
+
   def after_sign_in_path_for(resource)
     root_path
-  end  
-  
+  end
+
   def after_sign_out_path_for(resource)
     root_path
   end
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kane,:first_name_kana,:address,:porstal_code,:phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,:first_name_kana,:address,:porstal_code,:phone_number])
   end
 end
